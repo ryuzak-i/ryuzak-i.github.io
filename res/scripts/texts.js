e4aeedb30a608516010141cb8f63386a86f584d7
Ryuzaki <71939354+Ryuzak-i@users.noreply.github.com>
2024-08-07 16:35:21 +0300

    add languages switcher & ukrainian language + some css upgrades

--- res/scripts/texts.js
+++ res/scripts/texts.js
@@ -2,33 +2,50 @@ function loadConfig(config_path) {
     fetch(config_path).then(
         function (u) { return u.json(); }
     ).then(
-        function (json_config) {
-            loadTexts(json_config);
+        function (config) {
+            let default_language_id = config.TEXTS.DEFAULT_LANGUAGE;
+            loadTexts(config, default_language_id);
+
+            for (let language_id in config.TEXTS.LANGUAGES) {
+                lang_button = document.getElementById("language_" + language_id);
+
+                if (lang_button === null) { continue; }
+
+                lang_button.addEventListener("click", function () {
+                    loadTexts(config, language_id);
+                });
+            }
         }
     )
 }
 
+function loadTexts(config, language_id) {
+    if (current_language_id === language_id) {
+        return;
+    }
 
-function loadTexts(config) {
-    fetch(config.TEXTS.XML_DOCUMENT_PATH)
-    .then((response) => response.text())
-    .then((xml_string) => {
-        let xml_doc = new DOMParser().parseFromString(xml_string, "text/xml");
-        let texts_list = xml_doc.getElementsByTagName(config.TEXTS.XML_TAG_NAME);
+    current_language_id = language_id;
 
-        for (let i = 0; i < texts_list.length; i++) {
-            let text_id = texts_list[i].getAttribute(config.TEXTS.XML_ATTRIBUTE_ID_NAME)
-            let text_value = texts_list[i].getAttribute(config.TEXTS.XML_ATTRIBUTE_VALUE_NAME)
+    let xml_path = config.TEXTS.LANGUAGES[language_id].XML_DOCUMENT_PATH;
+    fetch(xml_path)
+        .then((response) => response.text())
+        .then((xml_string) => {
+            let xml_doc = new DOMParser().parseFromString(xml_string, "text/xml");
+            let texts_list = xml_doc.getElementsByTagName(config.TEXTS.XML_TAG_NAME);
 
-            let doc_element = document.getElementById(text_id);
+            for (let i = 0; i < texts_list.length; i++) {
+                let text_id = texts_list[i].getAttribute(config.TEXTS.XML_ATTRIBUTE_ID_NAME)
+                let text_value = texts_list[i].getAttribute(config.TEXTS.XML_ATTRIBUTE_VALUE_NAME)
 
-            if (doc_element && text_value) {
-                // console.log("For id '" + text_id + "' setup text '" + text_value + "'")
-                doc_element.innerHTML = text_value;
+                let doc_element = document.getElementById(text_id);
+
+                if (doc_element && text_value) {
+                    doc_element.innerHTML = text_value;
+                };
             };
-        }
-    });
+        });
 }
 
+let current_language_id;
 let config_path = "config.json";
-loadConfig(config_path)
+loadConfig(config_path);
